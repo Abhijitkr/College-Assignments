46ca53faca0a63fa39daf8e52451acf04aadd779
Abhijit Kumar Mahato <abhijitkr600@gmail.com>
2018-10-31 21:26:32 +0530

    break removed

--- Java/Assignment 1/9. Prime Number 1 to 1000/prime1to1000.java
+++ Java/Assignment 1/9. Prime Number 1 to 1000/prime1to1000.java
@@ -12,7 +12,6 @@ class prime1to1000
             {
                 if(i%j==0)
                 flag=1;
-                break;
             }
             if(flag==0)
             System.out.print(i+" ");
