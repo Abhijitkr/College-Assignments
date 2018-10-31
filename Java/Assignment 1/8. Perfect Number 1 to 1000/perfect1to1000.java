a788b1cd49af026236249f720392b93430baead0
Abhijit Kumar Mahato <abhijitkr600@gmail.com>
2018-10-31 20:44:51 +0530

    easy

--- Java/Assignment 1/8. Perfect Number 1 to 1000/perfect1to1000.java
+++ Java/Assignment 1/8. Perfect Number 1 to 1000/perfect1to1000.java
@@ -6,10 +6,10 @@ class perfect1to1000
         Scanner in = new Scanner(System.in);
         System.out.print("\f");
         System.out.println("The Perfect Numbers from 1 to 1000 are:-");
-        for(int i=2;i<1000;i++)
+        for(int i=1;i<=1000;i++)
         {
-            int s=1;
-            for(int j=2;j<i;j++)
+            int s=0;
+            for(int j=1;j<i;j++)
             {   
                 if(i%j==0)
                 s=s+j;                
