0a97e4744eb96fb41d6e439179057656f72a21df
Abhijit Kumar Mahato <abhijitkr600@gmail.com>
2018-11-01 10:29:05 +0530

    Correct prime

--- Python/Python Assignment/9. Prime Number 1 to 1000.py
+++ Python/Python Assignment/9. Prime Number 1 to 1000.py
@@ -1,12 +1,8 @@
-for n in range(2,1001):
+for n in range(1,1001):
     flag=0
-    for i in range(2,n//2+1):
+    for i in range(2,n+1):
         if(n%i==0):
             flag=flag+1
-    if(flag==0):
+    if(flag==1):
         print(n)
-
-
-
-    
-
+        
